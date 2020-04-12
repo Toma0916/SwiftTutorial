79ff361d7e281a719fecc813d4866ee5841f994a
鈴木透馬 <yiyiaxpb@gmail.com>
2020-04-12 17:12:27 +0900

    Hello World!

---
+++ SwiftTour/HelloWorld.swift
@@ -0,0 +1,6 @@
+// Tradition suggests that the first program in a new language should
+// print the words “Hello, world!” on the screen. In Swift, this can be done in a single line:
+
+// you can execute this script by runnning "swift hello_world.swift" in your console.
+print("Hello, World!")
+
